7cb5e81a7fa6446ba7cfc685e5ca3019d3a28cf8
JOSHI-PC\joshu <joshua.koester100@gmail.com>
2023-12-11 13:57:37 +0100

    update jointarray2string fcn

--- functions/jointarray2string.m
+++ functions/jointarray2string.m
@@ -1,37 +1,25 @@
 function jointstring = jointarray2string(jointarray)
     %JOINTARRAY2STRING convert a nx6 array with angle joint values to a nx1 
-    % string with the angle values in the form 'J(x,x,x,x,x,x)'
+    % string array with the angle values in the form 'J(x,x,x,x,x,x)'
 
     if size(jointarray,2) ~= 6
         error('expected input of cobotta angles is 6')
     end
     
-    if size(jointarray,1) == 1
-        jointstring = "J(";
+    jointstring = [];
+    for input= 1:size(jointarray,1)
         for angle = 1:6
-            jointstring = strcat(jointstring,num2str(jointarray(angle)));
+            if angle == 1
+                string = "J(";
+            end
+            string = strcat(string,num2str(jointarray(input,angle)));
             if angle ~= 6 
-                jointstring = strcat(jointstring,',');
+                string = strcat(string,',');
             else
-                jointstring = strcat(jointstring, ')');
+                string = strcat(string, ')');
             end
         end
-    else
-        jointstring = [];
-        for input= 1:size(jointarray,1)
-            for angle = 1:6
-                if angle == 1
-                    string = "J(";
-                end
-                string = strcat(string,num2str(jointarray(angle)));
-                if angle ~= 6 
-                    string = strcat(string,',');
-                else
-                    string = strcat(string, ')');
-                end
-            end
-            jointstring = [jointstring;string];
-        end
+        jointstring = [jointstring;string];
     end
          
 end
